da980a79fd91a2cdc197143e4cf7123109788607
Dzmitry.Auchynnikau <dzmitry.auchynnikau@itechart-group.com>
2021-12-09 20:44:05 +0300

    Dependecies are updated

--- cypress/integration/demo/transfers.spec.js
+++ cypress/integration/demo/transfers.spec.js
@@ -15,7 +15,7 @@ describe('Transfers scope <regression>', () => {
 
     it('sends transfer between accounts', () => {
         cy.visit('transfer/transfer-between-accounts')
-        transfersPage.getDebitFromDropDown().click()
+        transfersPage.getDebitFromDropDown().realHover().click()
         transfersPage.getDropDownOption().contains('EBQ11113487654').click()  
         transfersPage.getCreditToDropDown().click()
         transfersPage.getDropDownOption().contains('EBQ11223487456').click()
