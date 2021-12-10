022711da89ddc6252137a64cd18a1b4c8658dbf7
Dzmitry.Auchynnikau <dzmitry.auchynnikau@itechart-group.com>
2021-12-10 19:27:11 +0300

    cypress-terminal-report is added

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import '@bahmutov/cy-api/support'
+require('cypress-terminal-report/src/installLogsCollector')();
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
@@ -34,3 +35,4 @@ beforeEach(function() {
       this.skip();
     }
   })
+
