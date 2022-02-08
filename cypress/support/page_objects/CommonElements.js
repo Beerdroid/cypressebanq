996e882a81622e06750fe0090ca8fff11b83b8d7
Dzmitry.Auchynnikau <dzmitry.auchynnikau@itechart-group.com>
2022-02-08 11:26:06 +0300

    CommonElements.js is deleted

--- cypress/support/page_objects/CommonElements.js
+++
@@ -1,9 +0,0 @@
-export default class CommonElements {
-    static loadSpinner() {
-        return cy.get('div.spinner .la-ball-clip-rotate')
-    }
-
-    static messagesLink() {
-        return cy.get('[ng-reflect-router-link="/messages"]')
-    }
-}
